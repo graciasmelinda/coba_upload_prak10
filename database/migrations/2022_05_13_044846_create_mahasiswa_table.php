b61e9ba8a670fcb58094f2839799ff147d4a2b1a
graciasmelinda <gracia.sonia@si.ukdw.ac.id>
2022-06-19 19:28:55 +0700

    first commit

---
+++ database/migrations/2022_05_13_044846_create_mahasiswa_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('nim');
+            $table->string('nama',50);
+            $table->string('gender',20);
+            $table->string('jurusan',30);
+            $table->string('bidang_minat',30);
+            $table->timestamps();
+        });
+    } 
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+}
